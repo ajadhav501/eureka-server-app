a06a0d458819afcf6a2ccd82ef36d5cd0a6bdbeb
Abhijeet Jadhav <ajadhav50@yahoo.com>
2021-07-05 19:15:40 +0530

    first commit for eureka server

---
+++ src/test/java/com/epam/gpop/eurekaserverapp/EurekaServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.gpop.eurekaserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
